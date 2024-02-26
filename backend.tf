420cc32daad350ef2faad29679a01bdb0c825c3c
Robert Maury <daquinox@gmail.com>
2024-02-26 14:49:21 -0500

    Change Project and Namespace

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "firefly_gcp_sandbox_tfstate"
-    prefix  = "firefly-gcp-sandbox/state"
+    bucket  = "acme-terraform-state"
+    prefix  = "acme-gcp-prod/state"
   }
 }
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "three-tier-web-app" {
   source  = "GoogleCloudPlatform/three-tier-web-app/google"
   version = "0.1.8"
-  project_id  = "triple-bonito-415114"
+  project_id  = "acme-gcp-prod"
   region = "us-east1"
   zone = "us-east1-c"
 }
